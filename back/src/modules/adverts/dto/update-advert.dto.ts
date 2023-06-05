53b1b2244e6a92483d7a963ccf7a8f1763937b4d
Diego Guilherme <diegoguilherme752@oultook.com>
2023-06-05 12:49:25 -0300

    feat: function find all advert

--- back/src/modules/adverts/dto/update-advert.dto.ts
+++ back/src/modules/adverts/dto/update-advert.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAdvertDto } from './create-advert.dto';
+import { Fuel } from '../entities/advert.entity';
 
-export class UpdateAdvertDto extends PartialType(CreateAdvertDto) {}
+export class UpdateAdvertDto extends PartialType(CreateAdvertDto) {
+    fuel?: Fuel | String;
+}
