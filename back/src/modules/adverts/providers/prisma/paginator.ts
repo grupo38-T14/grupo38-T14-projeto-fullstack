01abd9fd76b35f4b61a72fd0843d3cb63b652755
Julia Pereira <juliapt95@gmail.com>
2023-06-12 13:13:07 -0300

    feat: cria filtro para preço e km

--- back/src/modules/adverts/providers/prisma/paginator.ts
+++ back/src/modules/adverts/providers/prisma/paginator.ts
@@ -25,12 +25,28 @@ export const paginator = (
     const page = Number(options?.page || defaultOptions?.page) || 1;
     const perPage = Number(options?.perPage || defaultOptions?.perPage) || 10;
     const year = Number(args?.where.year) || undefined;
+    const minPrice = Number(args?.where.price.lte) || undefined;
+    const maxPrice = Number(args?.where.price.gte) || undefined;
+    const minKM = Number(args?.where.km.lte) || undefined;
+    const maxKM = Number(args?.where.km.gte) || undefined;
 
     const skip = page > 0 ? perPage * (page - 1) : 0;
     const [total, data] = await Promise.all([
-      model.count({ where: { ...args.where, year: year } }),
+      model.count({
+        where: {
+          ...args.where,
+          year: year,
+          price: { lte: minPrice, gte: maxPrice },
+          km: { lte: minKM, gte: maxKM },
+        },
+      }),
       model.findMany({
-        where: { ...args.where, year: year },
+        where: {
+          ...args.where,
+          year: year,
+          price: { lte: minPrice, gte: maxPrice },
+          km: { lte: minKM, gte: maxKM },
+        },
         take: perPage,
         skip,
       }),
