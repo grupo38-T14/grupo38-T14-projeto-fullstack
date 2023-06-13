6989cb0e5d5044d20761659c490c14fdef7d7bfd
Lucas Mires <lucasmires10@gmail.com>
2023-06-13 15:21:15 -0300

    feat: usersCrud, routes

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdvertsModule } from './modules/adverts/adverts.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AdvertsModule],
+  imports: [AdvertsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
