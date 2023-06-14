48289e83ea011bac1be49d706761964b7fb0c7db
Julia Pereira <juliapt95@gmail.com>
2023-06-14 14:53:25 -0300

    feat: cria cards da página advert

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       gap: {
-        15: "3.75rem"
+        15: "3.75rem",
       },
       colors: {
         white: "#FFFFFF",
