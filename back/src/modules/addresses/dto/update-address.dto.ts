a54c291fde9e2fc3e32e4d74ff025df7d929d21a
Diego Guilherme <diegoguilherme752@oultook.com>
2023-06-19 14:40:42 -0300

    fix: partial types

--- back/src/modules/addresses/dto/update-address.dto.ts
+++ back/src/modules/addresses/dto/update-address.dto.ts
@@ -1,23 +1,5 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateAddressDto } from './create-address.dto';
 import { IsOptional } from 'class-validator';
 
-export class UpdateAddressDto extends PartialType(CreateAddressDto) {
-    @IsOptional()
-    cep: string
-
-    @IsOptional()
-    state: string
-
-    @IsOptional()
-    city: string
-
-    @IsOptional()
-    street: string
-
-    @IsOptional()
-    number: string
-
-    @IsOptional()
-    complement: string
-}   
+export class UpdateAddressDto extends PartialType(CreateAddressDto) {}   
