4552a107f9773a5928c3fe980dacce3c65f707ca
Diego Guilherme <diegoguilherme752@oultook.com>
2023-06-19 13:05:16 -0300

    feat: abstrat address update logic

--- back/src/modules/addresses/dto/update-address.dto.ts
+++ back/src/modules/addresses/dto/update-address.dto.ts
@@ -1,4 +1,23 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAddressDto } from './create-address.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateAddressDto extends PartialType(CreateAddressDto) {}
+export class UpdateAddressDto extends PartialType(CreateAddressDto) {
+    @IsOptional()
+    cep: string
+
+    @IsOptional()
+    state: string
+
+    @IsOptional()
+    city: string
+
+    @IsOptional()
+    street: string
+
+    @IsOptional()
+    number: string
+
+    @IsOptional()
+    complement: string
+}   
