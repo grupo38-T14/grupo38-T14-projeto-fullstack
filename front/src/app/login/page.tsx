5a9e2c3af46cdd0243fdc28a7a2382986810f28e
LuisFernandoFull <luisfernandomrs@outlook.com>
2023-06-21 18:49:53 -0300

    add modal recovery password

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+"use client";
 import Button from "@/components/button";
 import FormLogin from "@/components/forms/formLogin";
+import Modal from "@/components/Modal";
+import ModalRecoverPassword from "@/components/Modal/modalRecoverPassword";
+import React, { useState } from "react";
 
 export default function Login() {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-4">
+      {openModal && (
+        <Modal setOpenModal={setOpenModal}>
+          <ModalRecoverPassword setOpenModal={setOpenModal} />
+        </Modal>
+      )}
       <section className="flex flex-col gap-6 h-fit w-full lg:w-[25.75rem] p-8 rounded shadow bg-gray-100">
         <h5 className="mb-2">Login</h5>
-        <FormLogin />
+        <FormLogin setOpenModal={setOpenModal} />
         <span className="body-2 text-gray-20 text-center">
           Ainda não possui conta?
         </span>
