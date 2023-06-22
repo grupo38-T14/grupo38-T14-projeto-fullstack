5fa6d1a7f2e060f3556dcf1af4cd0071d2159877
LuisFernandoFull <luisfernandomrs@outlook.com>
2023-06-22 16:50:39 -0300

    add schema create new password

--- front/src/schemas/recoveryPassword.schema.ts
+++ front/src/schemas/recoveryPassword.schema.ts
@@ -6,5 +6,22 @@ export const RecoverySchema = z.object({
     .nonempty("E-mail é obrigatório")
     .email("Deve ser um e-mail válido"),
 });
+export const CreateNewPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, {
+        message: "A senha é obrigatória e precisa de mínimo 8 caracteres",
+      })
+      .regex(/(?=.*?[A-Z])/, "É necessário ao menos uma letra maiúscula")
+      .regex(/(?=.*?[a-z])/, "É necessário ao menos uma letra minúscula")
+      .regex(/(?=.*?[0-9])/, "É necessário pelo menos um número"),
+    confirm: z.string().min(1, "A confirmação de senha é obrigatória"),
+  })
+  .refine(({ password, confirm }) => password === confirm, {
+    message: "As senhas precisam corresponderem",
+    path: ["confirm"],
+  });
 
 export type RecoveryPasswordData = z.infer<typeof RecoverySchema>;
+export type CreateNewPasswordData = z.infer<typeof CreateNewPasswordSchema>;
