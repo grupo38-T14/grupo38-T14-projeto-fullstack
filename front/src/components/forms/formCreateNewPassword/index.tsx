79e9f3a558200315917a04c2ed8083208b0eccbd
LuisFernandoFull <luisfernandomrs@outlook.com>
2023-06-23 12:31:40 -0300

    created form new password

---
+++ front/src/components/forms/formCreateNewPassword/index.tsx
@@ -0,0 +1,82 @@
+import Button from "@/components/button";
+import Input from "@/components/inputs";
+
+import { RiLoader4Line } from "react-icons/ri";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  CreateNewPasswordSchema,
+  CreateNewPasswordData,
+} from "@/schemas/recoveryPassword.schema";
+
+const FormCreateNewPassword = () => {
+  const [btnLoading, setBtnLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty, isValid },
+  } = useForm<CreateNewPasswordData>({
+    resolver: zodResolver(CreateNewPasswordSchema),
+  });
+
+  const handleNewPassword = (data: CreateNewPasswordData) => {
+    console.log(data);
+    setBtnLoading(true);
+    setTimeout(() => {}, 1000);
+  };
+
+  return (
+    <>
+      <div className="">
+        <form
+          className="flex flex-col gap-6"
+          noValidate
+          onChange={handleSubmit(handleNewPassword)}
+        >
+          <Input
+            label="Digite seu nova senha "
+            placeholder="Senha"
+            type="password"
+            error={errors.password && errors.password.message}
+            register={register("password")}
+          />
+          <div className="flex flex-col font-sans text-sm font-medium leading-6">
+            <span>
+              Não ter letras e números repetidos ou sequenciais, ex: aaa,123
+            </span>
+            <span>Mínimo de 8 caracteres.</span>
+            <span>Mínimo de 1 elemento especial (#$%@)</span>
+            <span>Mínimo de 1 número.</span>
+            <span>Ter letras MAIÚSCULAS.</span>
+            <span>Ter letras minúsculas.</span>
+          </div>
+
+          <Input
+            label="Confime sua senha "
+            placeholder="Confirme sua senha "
+            type="password"
+            error={errors.confirm && errors.confirm.message}
+            register={register("confirm")}
+          />
+
+          <Button
+            type={!isDirty || !isValid ? "disableBland" : "brand"}
+            submit
+            disable={!isDirty || !isValid}
+          >
+            {!btnLoading ? (
+              "Redefinir senha"
+            ) : (
+              <RiLoader4Line size={30} color="#fff" className="animate-spin" />
+            )}
+          </Button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FormCreateNewPassword;
