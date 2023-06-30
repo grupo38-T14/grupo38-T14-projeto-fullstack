7077a21176a4d83750350e375e5ce39ebbcde2ef
LuisFernandoFull <luisfernandomrs@outlook.com>
2023-06-30 08:36:38 -0400

    add modal message

---
+++ front/src/components/Modal/modalMessageRecoverPassword/index.tsx
@@ -0,0 +1,12 @@
+const ModalMessageRecoverPassword = () => {
+  return (
+    <div className="flex flex-col text-center gap-6 h-fit w-full lg:w-[25.75rem] p-8 rounded shadow bg-gray-100">
+      <span className="h-7">Recupere a sua senha</span>
+      <p>
+        Caso o email email@email.com exista em nossa base, você receberá o link
+        para redefinição de senha!
+      </p>
+    </div>
+  );
+};
+export default ModalMessageRecoverPassword;
